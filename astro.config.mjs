415642fa1906364ef95f56989e8eaa7010ae46ab
robbryandev <114073557+robbryandev@users.noreply.github.com>
2024-12-12 17:22:11 -0800

    Add basic docs skeleton

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,42 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'LIMGEN',
+    customCss: [
+      './src/styles/global.css',
+    ],
+    social: {
+      github: 'https://github.com/limlabs/limgen',
+    },
+    logo: {
+      light: './src/assets/light-logo.svg',
+      dark: './src/assets/dark-logo.svg',
+    },
+    sidebar: [
+      {
+        label: 'Getting Started',
+        autogenerate: { directory: 'getting_started' },
+      },
+      {
+        label: 'Components',
+        autogenerate: { directory: 'components' },
+      },
+      {
+        label: 'Frameworks',
+        autogenerate: { directory: 'frameworks' }
+      },
+      {
+        label: 'Project Types',
+        autogenerate: { directory: 'project_types' },
+      },
+    ],
+  }),
+  tailwind({
+    // Disable the default base styles:
+    applyBaseStyles: false,
+  }),],
 });
